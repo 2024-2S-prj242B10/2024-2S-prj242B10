68b4de53323a2904e70fcf3e94625b93c27c8c0c
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2024-10-13 00:06:38 +0900

    Fix: 관리자 메뉴 출력 함수 삭제

--- manager/ui.py
+++ manager/ui.py
@@ -2,16 +2,6 @@ from manager.validation import *
 from src.user import *
 from src.book import *
 
-def admin_menu():
-    """관리자 메뉴"""
-    print("[관리자 메뉴]")
-    print("0. 로그아웃")
-    print("1. 도서 등록")
-    print("2. 도서 삭제")
-    print("3. 도서 검색")
-    print("4. 도서 조회")
-
-
 class Prompt:
 
     # 로그인 프롬프트
